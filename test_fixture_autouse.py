26333e69505a0bf2ff73a69dcad39cdac8817b66
hypno-spira <64416493+hypno-spira@users.noreply.github.com>
2021-04-01 21:41:22 +0700

    add comment for git check

--- test_fixture_autouse.py
+++ test_fixture_autouse.py
@@ -27,3 +27,5 @@ class TestMainPage1():
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+
+# comment for git check
